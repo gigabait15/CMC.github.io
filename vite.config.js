59caaf1adb721386173534f321a79571c3674ba4
gigabait15 <Workkerbecov@yandex.ru>
2024-10-15 01:04:02 +0300

    frontend

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+  },
   plugins: [react()],
   css: {
     postcss: {
